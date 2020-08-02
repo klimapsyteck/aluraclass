54655cef59529675e3c7c2c207bed899696cdca1
Kesley Lima <kesleylimasi@gmail.com>
2020-08-02 11:41:50 -0300

    dd

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -29,33 +29,33 @@ function CadastroCategoria() {
     );
   }
 
-  useEffect(() => {
-    if (window.location.href.includes('localhost')) {
-      const URL = 'http://aluraclass.herokuapp.com/categorias';
-      fetch(URL)
-        .then(async (respostaDoServer) => {
-          if (respostaDoServer.ok) {
-            const resposta = await respostaDoServer.json();
-            setCategorias(resposta);
-            return;
-          }
-          throw new Error('Não foi possível pegar os dados');
-        });
-    }
-    
-  }, []);
   // useEffect(() => {
-  //   const URL = window.location.hostname.includes('localhost')
-  //     ? 'http://localhost:8080/categorias'
-  //     : 'https://devsoutinhoflix.herokuapp.com/categorias';
-  //   fetch(URL)
-  //     .then(async (respostaDoServidor) => {
-  //       const resposta = await respostaDoServidor.json();
-  //       setCategorias([
-  //         ...resposta,
-  //       ]);
-  //   });
-  // });
+  //   if (window.location.href.includes('localhost')) {
+  //     const URL = 'http://aluraclass.herokuapp.com/categorias';
+  //     fetch(URL)
+  //       .then(async (respostaDoServer) => {
+  //         if (respostaDoServer.ok) {
+  //           const resposta = await respostaDoServer.json();
+  //           setCategorias(resposta);
+  //           return;
+  //         }
+  //         throw new Error('Não foi possível pegar os dados');
+  //       });
+  //   }
+    
+  // }, []);
+  useEffect(() => {
+    const URL = window.location.hostname.includes('localhost')
+      ? 'http://localhost:8080/categorias'
+      : 'https://devsoutinhoflix.herokuapp.com/categorias';
+    fetch(URL)
+      .then(async (respostaDoServidor) => {
+        const resposta = await respostaDoServidor.json();
+        setCategorias([
+          ...resposta,
+        ]);
+    });
+  });
 
   return (
     <PageDefault>
