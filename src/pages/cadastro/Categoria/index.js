6bf6b86f3f88b3bcd185a828782443b831c6dc9e
Kesley Lima <kesleylimasi@gmail.com>
2020-08-03 19:09:07 -0300

    cadastro de categoria

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,52 +1,32 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory} from 'react-router-dom';
 import PageDefault from '../../../componente/PageDefault';
 import FormField from '../../../componente/FormField';
 import Button from '../../../componente/Button';
 import useForm from '../../../hooks/useForm';
+import categoriasRepository from '../../../repositories/categorias';
 
 
 function CadastroCategoria() {
+  const history = useHistory();
   const valoresIniciais = {
     nome: '',
     descricao: '',
     cor: '',
+    url: '',
   };
 
-  const {handleChange, values, clearForm} = useForm(valoresIniciais);
+  // const {handleChange, values, clearForm} = useForm(valoresIniciais);
+  const {handleChange, values, clearForm} = useForm({
+    nome: '',
+    descricao: '',
+    cor: '',
+    url: '',
+});
 
-  const [categorias, setCategorias] = useState([]);
-  
+  const [categorias, setCategorias] = useState([]);  
 
-  
-  // feito pela imersão com minha alteração
-  // useEffect(() => {
-  //   if (window.location.href.includes('localhost')) {
-  //     const URL =  'http://localhost:8080/categorias';
-  //     fetch(URL)
-  //       .then(async (respostaDoServer) => {
-  //         if (respostaDoServer.ok) {
-  //           const resposta = await respostaDoServer.json();
-  //           setCategorias(resposta);
-  //           return;
-  //         }
-  //         throw new Error('Não foi possível pegar os dados');
-  //       });
-  //   }else {
-  //     const URL = 'https://aluraclass.herokuapp.com/categorias';
-  //     fetch(URL)
-  //       .then(async (respostaDoServer) => {
-  //         if (respostaDoServer.ok) {
-  //           const resposta = await respostaDoServer.json();
-  //           setCategorias(resposta);
-  //           return;
-  //         }
-  //         throw new Error('Não foi possível pegar os dados');
-  //       });
-  //   }
-    
-  // }, []);
-  // feito na aula gravada
+
   useEffect(() => {
     const URL = window.location.hostname.includes('localhost')
       ? 'http://localhost:8080/categorias'
@@ -58,21 +38,30 @@ function CadastroCategoria() {
           ...resposta,
         ]);
     });
-  });
+  }, []);
 
   return (
     <PageDefault>
       <h1>
         Cadastro de Categoria: {values.nome}  </h1>
 
-      <form onSubmit={function handleSubmit(infosDoEvento) {
+      <form onSubmit={(infosDoEvento) => {
         infosDoEvento.preventDefault();
-        setCategorias([
-          ...categorias,
-          values,
-        ]);
+        categoriasRepository.create({
+          titulo: values.nome,
+          cor: values.cor,
+          link_extra: {
+            "text": values.descricao, 
+             "url": values.url,
+          },
+      })
+      .then(() => {
+          console.log('Não sie');
+          history.push('/');
+      });
         clearForm();
       }} >
+        
 
         <FormField
           label="Nome da Categoria: "
@@ -95,6 +84,14 @@ function CadastroCategoria() {
           type="color"
           name="cor"
           value={values.cor}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="URL da categoria: "
+          type="text"
+          name="url"
+          value={values.url}
           onChange={handleChange}
         />
 
--- src/repositories/categorias.js
+++ src/repositories/categorias.js
@@ -2,6 +2,29 @@ import config from '../config';
 
     const URL_CATEGORIES = `${config.URL_BACKEND_TOP}/categorias`;
 
+    function create(objetoDaCategoria){
+      // console.log(config.URL_BACKEND_TOP);
+
+     return fetch(`${URL_CATEGORIES}?_embed=videos`, {
+         method: 'POST',  
+         headers: {
+              'Content-type': 'application/json',
+         },  
+         body: JSON.stringify(objetoDaCategoria),
+     })
+          .then(async (respostaDoServidor) => {
+              if (respostaDoServidor.ok){
+                  const resposta = await respostaDoServidor.json();
+                  return resposta;
+              }
+
+            throw new Error('Não foi possível cadastrar os dados :( ')
+
+          });       
+  }
+
+    // End my function to create a category
+
     function getAll() {
         return fetch(`${URL_CATEGORIES}`)
           .then(async (respostaDoServidor) => {
@@ -32,4 +55,5 @@ import config from '../config';
     export default{
         getAllWithVideos,
         getAll,
+        create,
     };
